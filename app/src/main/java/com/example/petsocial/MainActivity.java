31a03e71eebdb1f72c99590e9afff4e0c81f53e7
shivaansh81001 <shivaans@ualberta.ca>
2024-12-08 15:28:12 -0700

    Main activity xml

---
+++ app/src/main/java/com/example/petsocial/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.petsocial;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/example/petsocial/Post.java
@@ -0,0 +1,10 @@
+package com.example.petsocial;
+
+public class Post {
+
+    private int likes;
+    private int comments;
+
+    private Post() {
+    }
+}
